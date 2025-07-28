c26812b1302302fbbee862c88e9d55f42f7ede88
Manuel Kaufmann <humitos@gmail.com>
2025-07-28 09:40:08 +0200

    Add more comments to explain the code better

--- tasks.py
+++ tasks.py
@@ -5,6 +5,7 @@ BROKER_URL = "redis://:redispassword@cache:6379/0"
 app = Celery(
     'tasks',
     broker=BROKER_URL,
+    # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std-setting-worker_prefetch_multiplier
     worker_prefetch_multiplier=1,
 )
 
@@ -12,6 +13,9 @@ app = Celery(
 app.conf.broker_transport_options = {'visibility_timeout': 5}
 app.conf.result_backend_transport_options = {'visibility_timeout': 5}
 app.conf.visibility_timeout = 5
+
+# https://docs.celeryq.dev/en/stable/userguide/tasks.html#Task.acks_late
+app.conf.task_acks_late = True
 
 
 @app.task
--- trigger.py
+++ trigger.py
@@ -2,18 +2,27 @@ import time
 from tasks import example
 
 
+# This task should finish successfully
 example.apply_async(
     args=(3,),
     acks_late=True,
     time_limit=5,
 )
 
+# This task should hit time limit and finish unsuccessfully
 example.apply_async(
     args=(10,),
     acks_late=True,
     time_limit=5,
 )
 
+
+# The following tasks should be re-scheduled because it takes longer than
+# `visibility_timeout` and we have `acks_late=True`. However, **I suspect** that
+# Celery/Kombu is not respecting this `visibility_timeout` manual configuration
+# and always using the default value: 60 minutes.
+# The result is these tasks run completely and finish without issues
+# --I'm not expecting that, tho. I'm expecting them to be re-scheduled.
 example.apply_async(
     args=(30,),
     acks_late=True,
@@ -26,5 +35,13 @@ example.apply_async(
 
 example.apply_async(
     args=(90,),
+    acks_late=True,
+)
+
+
+# Triggering a task longer than 60 minutes, will hit the `visibility_timeout`
+# default value and it will be re-scheduled.
+example.apply_async(
+    args=(4500,),  # 1h15m
     acks_late=True,
 )
